48b43ad583528b0b2ac2ee9b4feb171d31e6f778
Mayura Bandara <128353336+SLxnoat@users.noreply.github.com>
2025-08-16 23:55:01 +0530

    0148_feature_pipeline

--- src/preprocessing/0148_feature_pipeline.py
+++ src/preprocessing/0148_feature_pipeline.py
@@ -1,23 +1,40 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "9b32e94b-ba16-4eb5-9069-d73204fee1b4",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "",
-   "name": ""
-  },
-  "language_info": {
-   "name": ""
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+from .text_processor import TextPreprocessor
+from .metadata_processor import MetadataProcessor
+
+class FeaturePipeline:
+    def __init__(self):
+        self.text_processor = TextPreprocessor()
+        self.metadata_processor = MetadataProcessor()
+        
+    def fit_transform(self, df):
+        """Complete feature engineering pipeline"""
+        df_processed = df.copy()
+        
+        # Text preprocessing
+        df_processed['statement_clean'] = self.text_processor.process_batch(
+            df_processed['statement'].fillna('')
+        )
+        
+        # Text feature engineering
+        df_processed = self.metadata_processor.engineer_text_features(df_processed)
+        
+        # Credibility features
+        df_processed = self.metadata_processor.create_credibility_features(df_processed)
+        
+        # Categorical encoding
+        categorical_cols = ['speaker', 'party_affiliation', 'subject', 'state_info', 'venue']
+        df_processed = self.metadata_processor.encode_categorical(
+            df_processed, categorical_cols
+        )
+        
+        # Numerical scaling
+        numerical_cols = [
+            'text_length', 'word_count', 'avg_word_length', 
+            'credibility_score', 'false_ratio'
+        ]
+        df_processed = self.metadata_processor.scale_numerical(
+            df_processed, numerical_cols
+        )
+        
+        return df_processed
